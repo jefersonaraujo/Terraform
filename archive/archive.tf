f801ac816b9b7c91f4159283f11ff2c9771f34d5
Jeferson Araujo <jefersonaraujo95@gmail.com>
2021-10-01 17:30:36 -0300

    init

---
+++ archive/archive.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/archive"
+      version = "2.2.0"
+    }
+  }
+}
+
+data "archive_file" "arquivozip" {
+  type = "zip"
+  source_dir =  "data_backup"
+  output_path = "backup_dir.zip"
+  
+}
+output "arquivozip" {
+  value = data.archive_file.arquivozip.output_size
+}
---
+++ local/random.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+resource "random_string" "random" {
+    length = 10
+    special = true
+  
+}
