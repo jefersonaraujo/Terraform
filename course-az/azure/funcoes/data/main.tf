3be1c3494d13506f1b9df9152eb665e203efb246
Jeferson Araujo <jefersonaraujo95@gmail.com>
2021-10-13 09:07:18 -0300

    for each

--- course-az/azure/funcoes/data/main.tf
+++ course-az/azure/funcoes/data/main.tf
@@ -5,8 +5,9 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "grupo-recurso" {
+  count = 2
   location = "brazilsouth"
-  name     = "rg-terraform"
+  name     = "rg-terraform-${count.index}"
   tags = {
     data        = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
     ambiente    = lower("Homologacao")
@@ -26,7 +27,7 @@ variable "vnetips" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnettreinamento"
   location            = "brazilsouth"
-  resource_group_name = "rg-terraform"
+  resource_group_name = "rg-terraform-1"
   address_space       = length(var.vnetips) == 0 ? ["10.0.0.0/16", "192.168.0.0/16"] : var.vnetips
   //address_space       = concat(var.vnetips, ["192.168.0.0/16"])
 
